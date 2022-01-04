8896ff25ebd14f23e2365e2c20cc9abeda4807fa
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-04 08:55:11 -0400

    Settle  AppRoute as a Parent Route and DashboardRoutes as a Child with protected routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,24 +1,17 @@
 // Mi Router Principal 
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
 // Hay que implementar el BrowserRouter desde el punto mas alto , para que funcione lso Routers 
-import { BrowserRouter } from "react-router-dom";
-import { MarvelScreen } from '../components/marvel/MarvelScreen';
-import { DcScreen } from '../components/dc/DcScreen';
 import { LoginScreen } from '../components/login/LoginScreen';
-import { SearchScreen } from '../components/search/SearchScreen';
-import { Navbar } from '../components/ui/NavBar';
+import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => {
    return (
       <BrowserRouter>
-         <Navbar/>
          <Routes>
-            <Route path="/" element={<MarvelScreen />} />
-            <Route path="/marvel" element={<MarvelScreen />} />
-            <Route path="/dc" element={<DcScreen />} />
-            <Route path="/search" element={<SearchScreen />} />
             <Route path="/login" element={<LoginScreen />} />
+            <Route path="/*" element={<DashboardRoutes />} />
          </Routes>
       </BrowserRouter>
    )
---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import { Navbar } from '../components/ui/NavBar';
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { DcScreen } from '../components/dc/DcScreen';
+import { SearchScreen } from '../components/search/SearchScreen';
+
+export const DashboardRoutes = () => {
+   return (
+      <>
+         <Navbar />
+         <Routes>
+            <Route path="marvel" element={<MarvelScreen />} />
+            <Route path="dc" element={<DcScreen />} />
+
+            <Route path="search" element={<SearchScreen />} />
+
+            <Route path="/" element={<MarvelScreen />} />
+         </Routes>
+      </>
+   )
+}
