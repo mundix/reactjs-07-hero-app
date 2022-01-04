d9967022aaa9f32b97a01e7321e0d39affc2625e
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-04 08:59:55 -0400

    Created a HeroScreen component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,7 +2,8 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
-// Hay que implementar el BrowserRouter desde el punto mas alto , para que funcione lso Routers 
+// Hay que implementar el BrowserRouter desde el punto mas alto , para que funcione lso Routers 3
+// Y no es necesario colocarlo en las routas hijas. 
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
 
--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -3,6 +3,7 @@ import { Navbar } from '../components/ui/NavBar';
 import { MarvelScreen } from '../components/marvel/MarvelScreen';
 import { DcScreen } from '../components/dc/DcScreen';
 import { SearchScreen } from '../components/search/SearchScreen';
+import { HeroScreen } from '../components/hero/HeroScreen';
 
 export const DashboardRoutes = () => {
    return (
@@ -13,6 +14,7 @@ export const DashboardRoutes = () => {
             <Route path="dc" element={<DcScreen />} />
 
             <Route path="search" element={<SearchScreen />} />
+            <Route path="hero" element={<HeroScreen />} />
 
             <Route path="/" element={<MarvelScreen />} />
          </Routes>
