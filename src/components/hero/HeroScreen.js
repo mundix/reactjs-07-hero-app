d2f2b3d68f59ec19c102daef49b3648c39d5af07
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-05 04:18:23 -0400

    Update add login to HeroScreen and if hero no exist redirect to /

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Navigate, useParams } from "react-router-dom"
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+
+   // Esto es nuevo 
+   const params = useParams();
+   // console.log(params.heroeId);
+   const hero = getHeroById(params.heroeId);
+
+   if(!hero) {
+      return <Navigate to='/'/>
+   }
+
    return (
       <div>
          <h1>Hero Screen</h1>
+         <p>{hero.superhero}</p>
       </div>
    )
 }
