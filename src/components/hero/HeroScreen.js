c28e5902cfd687263623bee86519e2b7b820ed8d
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-05 04:35:34 -0400

    Updated Layout and implemented useParam  and useNavigate ro return

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,21 +1,60 @@
-import { Navigate, useParams } from "react-router-dom"
+import { Navigate, useParams, useNavigate } from "react-router-dom"
 import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+
+   const navigate = useNavigate();
 
    // Esto es nuevo 
    const params = useParams();
    // console.log(params.heroeId);
    const hero = getHeroById(params.heroeId);
 
-   if(!hero) {
-      return <Navigate to='/'/>
+   if (!hero) {
+      return <Navigate to='/' />
+   }
+
+   const { id,
+      superhero,
+      publisher,
+      alter_ego,
+      first_appearance,
+      characters } = hero;
+
+   const imagePath = `/assets/${id}.jpg`;
+
+   const handleReturn = () => {
+      navigate(-1);
    }
 
    return (
-      <div>
-         <h1>Hero Screen</h1>
-         <p>{hero.superhero}</p>
+      <div className="row mt-5">
+         <div className="col-4">
+            <img
+               src={imagePath}
+               alt={superhero}
+               className="img-thumbnail"
+            />
+         </div>
+         <div className="col-8">
+            <h3>{superhero}</h3>
+            <ul className="list-group ">
+               <li className="list-group-item"><b>Alter ego</b> {alter_ego}</li>
+               <li className="list-group-item"><b>Publisher</b> {publisher}</li>
+               <li className="list-group-item"><b>First Appereance</b> {first_appearance}</li>
+            </ul>
+
+            <h5>Characters</h5>
+            <p>{characters}</p>
+
+            <button
+               className="btn btn-outline-primary"
+               onClick={() => handleReturn()}
+            >
+               Regresar
+            </button>
+
+         </div>
       </div>
    )
 }
