adcdb41a6064fb352359cff782292d809908bc63
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-07 05:19:39 -0400

    Created AuthContext file

---
+++ src/auth/authContext.js
@@ -0,0 +1,8 @@
+//Aqui se declara diferente  se pone en Mayuscula la primer letra del nombre del Context AuthContext 
+
+import { createContext } from "react";
+
+
+
+
+export const AuthContext = createContext();
