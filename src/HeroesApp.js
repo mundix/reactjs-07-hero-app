9c4e19edeab1433d45de17311f4ec87eca50f320
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-08 08:37:34 -0400

    Updated implemented useReducer on HeroApp.

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,16 +1,27 @@
 import { useReducer } from "react"
 import { AuthContext } from "./auth/authContext"
 import { AppRouter } from "./routers/AppRouter"
+import { authReducer } from './auth/authReducer';
+
+const init = () => {
+   return {
+      logged: true,
+      name: 'Edmundo Temporal'
+   }   
+}
+
+
 
 export const HeroesApp = () => {
 
-   const user = {
-      name: 'Edmundo',
-      logged: true
-   }
-   const {state, dispatch} = useReducer();
+   
+   const [user, dispatch] = useReducer(authReducer, {}, init);
+
    return (
-      <AuthContext.Provider value={user}>
+      <AuthContext.Provider value={{
+         user,
+         dispatch
+      }}>
          <AppRouter />
       </AuthContext.Provider>
    )
