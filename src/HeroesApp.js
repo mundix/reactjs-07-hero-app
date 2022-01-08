fc7d5a7bd1d68b3e19844458a5a63fbc5eeb1de3
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-08 09:26:03 -0400

    Update Parse JSON the localStorage

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -4,7 +4,7 @@ import { AppRouter } from "./routers/AppRouter"
 import { authReducer } from './auth/authReducer';
 
 const init = () => {
-   return localStorage.getItem('user') || { logged: false};
+   return JSON.parse(localStorage.getItem('user')) || { logged: false};
 }
 
 export const HeroesApp = () => {
