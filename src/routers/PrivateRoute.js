d64fd7657d313d106ecb77af2f17e6c15cef9ca1
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-18 03:32:18 -0400

    ...

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -3,10 +3,8 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../auth/authContext"
 
 export const PrivateRoute = ({ children}) => {
-
+Î
    const {user} = useContext(AuthContext);
-
-   
 
    return user.logged ? children : <Navigate to="/login"/>
 }
