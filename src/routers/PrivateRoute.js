e8a350ee664c07a661afd85dc04da98d9b62398f
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-18 03:32:48 -0400

    Removed character

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../auth/authContext"
 
 export const PrivateRoute = ({ children}) => {
-Î
+
    const {user} = useContext(AuthContext);
 
    return user.logged ? children : <Navigate to="/login"/>
