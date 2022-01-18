923de4701f2d6291752d9c7a5360d96d2399d222
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-01-18 03:22:03 -0400

    Updated and implemented private route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,13 +6,19 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 // Y no es necesario colocarlo en las routas hijas. 
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
    return (
       <BrowserRouter>
          <Routes>
             <Route path="/login" element={<LoginScreen />} />
-            <Route path="/*" element={<DashboardRoutes />} />
+            <Route path="/*" element={
+               <PrivateRoute>
+                  <DashboardRoutes />
+               </PrivateRoute>
+            } />
+            {/* <Route path="/*" element={<DashboardRoutes />} /> */}
          </Routes>
       </BrowserRouter>
    )
